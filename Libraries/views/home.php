b112f3e933df345a6ef77009b81238f92207e792
warhog76 <warhog76@free.fr>
2022-04-28 10:18:10 +0200

    Update 02 - Routing files and updated files

---
+++ Libraries/views/home.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * CE FICHIER A POUR BUT D'AFFICHER LA PAGE D'ACCUEIL !
+ *
+ * On va donc se connecter à la base de données, récupérer les 2 derniers articles du plus récent au plus ancien
+ * puis on va boucler dessus pour afficher chacun d'entre eux
+
+ */
+
+use controllers\Article;
+
+require_once('libraries/database.php');
+require_once('libraries/controllers/Article.php');
+
+$controller = new Article();
+$controller->index();
