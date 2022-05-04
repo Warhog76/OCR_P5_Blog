8e77c2e8d3a78b85ec6ff4bea687033ac27c3c7b
warhog76 <warhog76@free.fr>
2022-05-04 18:21:48 +0200

    Update 03 - Refactoring of all files and autoload

--- src/App/Models/Model.php
+++ src/App/Models/Model.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use App\Controllers\Database;
+use App\Models\Database;
 
 abstract class Model
 {
@@ -16,25 +16,13 @@ abstract class Model
     }
 
     /**
-     * find
-     */
-    public function find(int $id) : array
-    {
-
-        $query = $this->pdo->prepare("SELECT * FROM {$this->table} WHERE 'id' = :id");
-        $query->execute(['id' => $id]);
-        $item = $query->fetch();
-
-        return $item;
-    }
-
-    /**
      * delete
+     *
+     *    public function delete(int $id) :void
+     *    {
+     *
+     *        $query = $this->pdo->prepare("DELETE FROM {$this->table} WHERE id = :id");
+     *        $query->execute(['id' => $id]);
+     *    }
      */
-    public function delete(int $id) :void
-    {
-
-        $query = $this->pdo->prepare("DELETE FROM {$this->table} WHERE id = :id");
-        $query->execute(['id' => $id]);
-    }
 }
