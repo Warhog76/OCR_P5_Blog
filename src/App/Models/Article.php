9bf931872125732bd3f311024768d5426092646d
warhog76 <warhog76@free.fr>
2022-05-13 10:11:40 +0200

    Update 04 - New system with models objects

--- src/App/Models/Article.php
+++ src/App/Models/Article.php
@@ -47,37 +47,123 @@ Class Article
         return $this->id;
     }
 
+    /**
+     * @param int $id
+     * @return Article
+     */
+    public function setId(int $id): Article
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getTitle(): string
     {
         return $this->title;
     }
 
+    /**
+     * @param string $title
+     * @return Article
+     */
+    public function setTitle(string $title): Article
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getContent(): string
     {
         return $this->content;
     }
 
+    /**
+     * @param string $content
+     * @return Article
+     */
+    public function setContent(string $content): Article
+    {
+        $this->content = $content;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getImage(): string
     {
         return $this->image;
     }
 
+    /**
+     * @param string $image
+     * @return Article
+     */
+    public function setImage(string $image): Article
+    {
+        $this->image = $image;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
     public function getWriter(): string
     {
         return $this->writer;
     }
 
-    public function getCreated()
+    /**
+     * @param string $writer
+     * @return Article
+     */
+    public function setWriter(string $writer): Article
+    {
+        $this->writer = $writer;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDate(): string
     {
         return $this->date;
     }
 
+    /**
+     * @param string $date
+     * @return Article
+     */
+    public function setDate(string $date): Article
+    {
+        $this->date = $date;
+        return $this;
+    }
 
+    /**
+     * @return int
+     */
     public function getPosted(): int
     {
         return $this->posted;
     }
 
+    /**
+     * @param int $posted
+     * @return Article
+     */
+    public function setPosted(int $posted): Article
+    {
+        $this->posted = $posted;
+        return $this;
+    }
 
 
 
